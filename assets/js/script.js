d081a10a6ab5066f74fada8f8a35ffc92957e258
Hailin Ruan <hlruan829@gmail.com>
2023-07-17 23:19:27 +1000

    added javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 const APIKey = "bf18cdbcb62244f196c3b93693db628b";
-const queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-let city;
+const APIUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-fetch(queryUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('Fetch Response \n-------------');
+const searchInput = document.querySelector('#searchInput');
+const SearchBtn = document.querySelector('#button');
+
+async function checkWeather(city){
+    const response = await fetch(APIUrl + city + `&appid=${APIKey}`);
+    var data = await response.json();
+
     console.log(data);
-  });
+
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+}
+
+SearchBtn.addEventListener("click", ()=>{
+    checkWeather(searchInput.value);
+})
+
+// const weatherBox = document.querySelector('#weather-right-side')
+// const WeatherCurrent = document.querySelector('.weather-current');
+// const weatherDetails = document.querySelector('.weather-details')
+// const errorMessage = document.querySelector('.not-found')
