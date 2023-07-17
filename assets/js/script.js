5921f132d40e3578a6325d356cae1f866d21295d
Hailin Ruan <hlruan829@gmail.com>
2023-07-18 02:09:02 +1000

    clear saved searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,7 @@ function displaySavedSearches() {
     searchEntry.appendChild(removeButton);
 
     searchEntry.addEventListener("click", () => {
-      checkWeather(searchValue);
+      searchWeather(searchValue);
     });
 
     savedSearchesContainer.appendChild(searchEntry);
@@ -59,65 +59,77 @@ function clearSavedSearches() {
   savedSearchesContainer.innerHTML = "";
 }
 
-SearchBtn.addEventListener("click", saveInputToLocalStorage);
+SearchBtn.addEventListener("click", () => {
+  saveInputToLocalStorage();
+  searchWeather(searchInput.value);
+});
 clearSavedSearchesBtn.addEventListener("click", clearSavedSearches);
 
-window.addEventListener("DOMContentLoaded", displaySavedSearches);
-
-const savedInputValue = localStorage.getItem("searchInputValue");
-if (savedInputValue) {
-  searchInput.value = savedInputValue;
-}
-
-async function checkWeather(city) {
-  try {
-    const response = await fetch(APIUrl + city + `&appid=${APIKey}`);
-    if (response.status === 404) {
-      document.querySelector(".not-found").style.display = "block";
-      setTimeout(function () {
-        document.querySelector(".not-found").style.display = "none";
-      }, 3000);
-      document.querySelector(".weather").style.display = "none";
-    } else {
-      const data = await response.json();
-
-      document.querySelector(".city").innerHTML = data.name;
-      document.querySelector(".date").innerHTML = dayjs().format("D MMMM YYYY");
-      document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-      document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-      const weatherMain = data.weather[0].main;
-
-      weatherIcons.forEach(icon => {
-        let weatherIconClass = "";
-
-        if (weatherMain === "Clouds") {
-          weatherIconClass = "wi-cloudy";
-        } else if (weatherMain === "Clear") {
-          weatherIconClass = "wi-day-sunny";
-        } else if (weatherMain === "Rain") {
-          weatherIconClass = "wi-rain";
-        } else if (weatherMain === "Drizzle") {
-          weatherIconClass = "wi-showers";
-        } else if (weatherMain === "Snow") {
-          weatherIconClass = "wi-snow";
-        } else if (weatherMain === "Thunderstorm") {
-          weatherIconClass = "wi-thunderstorm";
-        } else {
-          weatherIconClass = "wi-na"; // Default icon if weather condition doesn't match any specific condition
-        }
-
-        icon.classList.add(weatherIconClass);
-      });
-
-      document.querySelector(".weather").style.display = "block";
-    }
-  } catch (error) {
-    console.error(error);
-    // Handle the error or display an appropriate error message
+window.addEventListener("DOMContentLoaded", () => {
+  displaySavedSearches();
+  const savedInputs = JSON.parse(localStorage.getItem("savedSearches")) || [];
+  if (savedInputs.length > 0) {
+    const lastSavedInput = savedInputs[savedInputs.length - 1];
+    searchInput.value = lastSavedInput;
+    searchWeather(lastSavedInput);
   }
-}
+});
+
+a// ...
+
+async function searchWeather(city) {
+    try {
+      const response = await fetch(APIUrl + city + `&appid=${APIKey}`);
+      if (response.status === 404) {
+        document.querySelector(".not-found").style.display = "block";
+        setTimeout(function () {
+          document.querySelector(".not-found").style.display = "none";
+        }, 3000);
+        document.querySelector(".weather").style.display = "none";
+      } else {
+        const data = await response.json();
+  
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".date").innerHTML = dayjs().format("D MMMM YYYY");
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+  
+        const weatherMain = data.weather[0].main;
+  
+        weatherIcons.forEach(icon => {
+            let weatherIconClass = "";
+          
+            if (weatherMain === "Clouds") {
+              weatherIconClass = "wi-cloudy";
+            } else if (weatherMain === "Clear") {
+              weatherIconClass = "wi-day-sunny";
+            } else if (weatherMain === "Rain") {
+              weatherIconClass = "wi-rain";
+            } else if (weatherMain === "Drizzle") {
+              weatherIconClass = "wi-showers";
+            } else if (weatherMain === "Snow") {
+              weatherIconClass = "wi-snow";
+            } else if (weatherMain === "Thunderstorm") {
+              weatherIconClass = "wi-thunderstorm";
+            } else {
+              weatherIconClass = "wi-na"; // Default icon if weather condition doesn't match any specific condition
+            }
+          
+            icon.className = "wi " + weatherIconClass;
+          });
+          
+  
+        document.querySelector(".weather").style.display = "block";
+      }
+    } catch (error) {
+      console.error(error);
+      // Handle the error or display an appropriate error message
+    }
+  }
+  
+  // ...
+  
 
 SearchBtn.addEventListener("click", () => {
   checkWeather(searchInput.value);
